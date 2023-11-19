00a4ec7fca07b9346584e9550be51628642c3de9
trippieb <137955370+trippieb@users.noreply.github.com>
2023-11-19 11:02:58 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(db,"mongodb+srv://Heal:8onX1VOZNwAIoXip@cluster0.hjewe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
