1ccc8a8beca71a4771508957e85b74fa0ff2fcda
trippieb <137955370+trippieb@users.noreply.github.com>
2023-11-19 11:06:48 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongodb+srv://Heal:8onX1VOZNwAIoXip@cluster0.hjewe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+const mongoose = require("mongoose")
 const Schema1 = mongoose.Schema;
 
 const user1schema = new Schema1({
